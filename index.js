d16e5aa0dfedf5d06e8da04ba0bc4d2c5a4dd6eb
HOWARD1021 <44057435+HOWARD1021@users.noreply.github.com>
2019-04-23 16:49:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 
 
 var score = 100
-// var message
+//( 在if 後面沒有打上空格導致不管score怎麼打,都是呈現A)
 if (score >= 100){
 	grade = "A"
 }   else if (score > 60) {	
@@ -22,3 +22,5 @@ if (score >= 100){
 }
 
 console.log(grade)
+
+
