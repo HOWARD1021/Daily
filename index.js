fa6612de0071525909a94e241741e1bdeabafe54
Howard <ms04626730@gmail.com>
2019-04-23 16:44:49 +0800

    index01

---
+++ index.js
@@ -0,0 +1,24 @@
+// var score = 66
+
+
+// if (score >= 100){
+// 	console.log ("A")
+// }   else if (score > 60) {	
+// 	console.log ("pass")
+// }	else {
+// 	console.log("fail")
+// }
+
+
+
+var score = 100
+// var message
+if (score >= 100){
+	grade = "A"
+}   else if (score > 60) {	
+	grade= "pass"
+}	else {
+	grade="fail"
+}
+
+console.log(grade)
